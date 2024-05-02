f8bdee40cc2788040f315be2aa8a9b2b0777dcef
OJOJIN <ojy101039@gmail.com>
2024-05-02 15:33:18 +0900

    [feat] SearchController

--- src/main/java/org/bootstrap/search/controller/SearchController.java
+++ src/main/java/org/bootstrap/search/controller/SearchController.java
@@ -1,4 +1,26 @@
 package org.bootstrap.search.controller;
 
+
+import lombok.RequiredArgsConstructor;
+import org.bootstrap.search.common.SuccessCode;
+import org.bootstrap.search.common.SuccessResponse;
+import org.bootstrap.search.dto.support.PostsDto;
+import org.bootstrap.search.service.SearchService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/search")
 public class SearchController {
+
+    private final SearchService searchService;
+
+    @GetMapping("/posts")
+    public ResponseEntity<SuccessResponse<?>> findPostByTitle(@RequestParam String title) {
+        return SuccessResponse.ok(searchService.findPostByTitle(title));
+    }
 }
