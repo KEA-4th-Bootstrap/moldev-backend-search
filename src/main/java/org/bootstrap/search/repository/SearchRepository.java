85dadb22b2192b5c697397f2ac12fae8b1eb55a4
OJOJIN <ojy101039@gmail.com>
2024-05-02 15:32:52 +0900

    [feat] searchRepository

--- src/main/java/org/bootstrap/search/repository/SearchRepository.java
+++ src/main/java/org/bootstrap/search/repository/SearchRepository.java
@@ -1,6 +1,6 @@
 package org.bootstrap.search.repository;
 
-import org.bootstrap.search.document.Post;
+import org.bootstrap.search.dto.support.PostsDto;
 import org.springframework.stereotype.Repository;
 
 import java.io.IOException;
@@ -8,6 +8,6 @@ import java.io.IOException;
 @Repository
 public interface SearchRepository {
 
-    public Post findPostByText(String text) throws IOException;
+    PostsDto findPostByTitle(String title) throws IOException;
 
 }
