c2885240a9f5d3b9085ee17af6b903eea83af963
OJOJIN <ojy101039@gmail.com>
2024-05-02 15:33:09 +0900

    [feat] SearchService

--- src/main/java/org/bootstrap/search/service/SearchService.java
+++ src/main/java/org/bootstrap/search/service/SearchService.java
@@ -1,4 +1,18 @@
 package org.bootstrap.search.service;
 
+
+import lombok.RequiredArgsConstructor;
+import org.bootstrap.search.dto.support.PostsDto;
+import org.bootstrap.search.helper.SearchHelper;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class SearchService {
+
+    private final SearchHelper searchHelper;
+
+    public PostsDto findPostByTitle(String title) {
+        return searchHelper.findPostsByTitle(title);
+    }
 }
