02c7228f8609412c5457df7246160238b53ca6ee
OJOJIN <ojy101039@gmail.com>
2024-05-03 14:34:17 +0900

    KBB-60 [feat] Post index

--- src/main/java/org/bootstrap/search/common/Constants.java
+++ src/main/java/org/bootstrap/search/common/Constants.java
@@ -1,5 +1,5 @@
 package org.bootstrap.search.common;
 
 public class Constants {
-    public static final String POSTS = "moldb-test-post";
+    public static final String POSTS = "moldb-post-test";
 }
