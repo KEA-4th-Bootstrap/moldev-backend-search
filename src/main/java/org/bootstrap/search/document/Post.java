91b4be4bf6a2a9409c69a84521028559a392b377
OJOJIN <ojy101039@gmail.com>
2024-05-20 12:57:52 +0900

    KBB-107 [feat] 게시글에 맞게 응답값 변경

--- src/main/java/org/bootstrap/search/document/Post.java
+++ src/main/java/org/bootstrap/search/document/Post.java
@@ -4,21 +4,19 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public record Post (
-        String Table,
-        String post_state,
-        String similarity_key_type,
-        String important_key_type,
-        String last_modified_date,
         String title,
         String content,
-        String dormitory_type,
         String post_id,
-        String user_id,
+        String member_id,
+        String thumbnail,
+        String Table,
+        String last_modified_date,
         String id,
+        String category,
         String create_date,
-        String end_date,
         String _timestamp,
-        String room_type
+        String view_count,
+        String moldev_id
 ){
 
 }
