82c862d964ebd46126401fee5cedbd5eff5b96f9
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-21 09:56:09 +0900

    [fix] composer를 위한 response 형식 수정

--- src/main/java/org/bootstrap/search/controller/SearchController.java
+++ src/main/java/org/bootstrap/search/controller/SearchController.java
@@ -2,10 +2,10 @@ package org.bootstrap.search.controller;
 
 
 import lombok.RequiredArgsConstructor;
-import org.bootstrap.search.common.SuccessResponse;
+import org.bootstrap.search.dto.response.SearchPostResponseDto;
 import org.bootstrap.search.service.SearchService;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +20,8 @@ public class SearchController {
     private final SearchService searchService;
 
     @GetMapping("/posts")
-    public ResponseEntity<SuccessResponse<?>> findPostByTitle(@RequestParam final String title,
-                                                              final Pageable pageable) {
-        return SuccessResponse.ok(searchService.findPostByTitle(title, pageable));
+    public ResponseEntity<SearchPostResponseDto> findPostByTitle(@RequestParam final String title,
+                                                                 final Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.OK).body(searchService.findPostByTitle(title, pageable));
     }
 }
